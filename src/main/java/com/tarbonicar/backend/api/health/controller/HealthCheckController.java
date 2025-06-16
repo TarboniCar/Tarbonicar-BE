08ede302a8715143907e2eaf7a39f0f5dc1bf3dd
rhkr8521 <rhkr8521@gmail.com>
2025-06-16 12:49:43 +0900

    [SETTING] HealthCheckController 등록

--- src/main/java/com/tarbonicar/backend/api/health/controller/HealthCheckController.java
+++ src/main/java/com/tarbonicar/backend/api/health/controller/HealthCheckController.java
@@ -1,9 +1,12 @@
 package com.tarbonicar.backend.api.health.controller;
 
+import com.tarbonicar.backend.common.exception.BadRequestException;
 import com.tarbonicar.backend.common.response.ApiResponse;
 import com.tarbonicar.backend.common.response.SuccessStatus;
+import com.tarbonicar.backend.common.response.ErrorStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +24,17 @@ public class HealthCheckController {
     public ResponseEntity<ApiResponse<String>> healthCheckData() {
 
         return ApiResponse.success(SuccessStatus.SEND_HEALTH_SUCCESS, "OK");
+    }
+
+    // 예외처리 예제
+    @GetMapping("/exception-test/{data}")
+    public ResponseEntity<ApiResponse<Void>> healthCheckData(@PathVariable("data") String data) {
+
+        if (data.equals("run")){
+            // 커스텀 예외처리(BadRequstException) 사용방법 및 ErrorStatus 사용방법
+            throw new BadRequestException(ErrorStatus.VALIDATION_REQUEST_MISSING_EXCEPTION.getMessage());
+        }
+
+        return ApiResponse.success_only(SuccessStatus.SEND_HEALTH_SUCCESS);
     }
 }
